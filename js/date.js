94397ceb1d808da0a29ce2d654f974cbe4b38637
gpdnjs110415 <50328310+gpdnjs110415@users.noreply.github.com>
2020-08-27 00:24:54 +0900

    init

---
+++ js/date.js
@@ -0,0 +1,159 @@
+let days = ['일', '월', '화', '수', '목', '금', '토'];
+let period = ['조회', '1교시', '2교시', '3교시', '4교시', '점심시간', '5교시', '6교시', '7교시', '종례', '자습'];
+let teacher = ['김태', '이효', '장문', '이경', '함지', '김종', '정지', '장재', '조혜', '이재', '원미', '박경', '박성', '조혜', '동아리', '김태', '김태', '김태', '', ''];
+let zoomId = ['8134411742', '7987833794', '7965443330', '7126847915', '6863031340', '718015132', '7722593481', '7223454774', '3650046422', '7864104388', '5977904321', '3955689232', '3335559622', '3650046422', '', '8134411742', '8134411742', '8134411742', '', '#'];
+let subject = ['문학', '공업수학', '수학2', '영어1', '응용프로그래밍 개발 / 정보보호 관리', '화학1', '중국어1', '성공적인 직업생활', '응용프로그래밍 화면구현', '운동과 건강', '정보보호 관리', '진로', '정보통신', '자료구조', 'CA', 'HR', '', '', '점심시간', ''];
+
+//[요일][교시]
+let timetable = [
+    ['19', '19', '19', '19', '19', '19', '19', '19', '19', '19', '19'],
+    ['16', '7', '5', '9', '4', '18', '4', '3', '12', '17', '19'],
+    ['16', '13', '1', '7', '0', '18', '2', '11', '14', '17', '19'],
+    ['16', '5', '8', '1', '2', '18', '6', '3', '17', '17', '19'],
+    ['16', '8', '0', '9', '13', '18', '5', '11', '15', '17', '19'],
+    ['16', '12', '4', '4', '1', '18', '6', '5', '17', '19'],
+    ['19', '19', '19', '19', '19', '19', '19', '19', '19', '19', '19']
+];
+
+//zoom 링크
+let zoom = 'https://zoom.us/j/' //zoom기본 링크
+
+let korean = zoom + '8134411742'; //김태철 0
+let math_g = zoom + '7987833794'; //이효현 1
+let math = zoom + '7965443330'; //장문석 2
+let english = zoom + '7126847915'; //이경은 3
+let java = zoom + '6863031340'; // 함지연 4
+let science = zoom + '4361766687'; // 김종수 5
+let china = zoom + '7722593481'; // 정지훈 6
+let job = zoom + '7223454774'; //장재원 7
+let data_web = zoom + '3650046422'; //조혜연 8
+let pe = zoom + '7864104388'; //이재민 9
+let defence = zoom + '5977904321'; //원미경 10
+let dream = zoom + '3955689232'; //박경수 11
+let infom_t = zoom + '3335559622'; //박성수 12
+
+
+function getTimeInfo() {
+    let today = new Date();
+    let year = today.getFullYear();
+    let month = today.getMonth() + 1;
+    let date = today.getDate();
+    let day = days[today.getDay()];
+    let hour = today.getHours();
+    let minute = today.getMinutes();
+    let second = today.getSeconds();
+
+    //몇시?
+    today = year + `년 ` + month + `월 ` + date + `일 ` + day + `요일`;
+    document.getElementById('date').innerText = today;
+    document.getElementById('now').innerText = hour + '시 ' + minute + '분 ' + second + '초';
+
+
+    //몇교시?
+    let time;
+    if (hour == 8) {
+        if (minute <= 55) {
+            time = 0;
+        } else {
+            time = 1;
+        }
+    } else if (hour == 9) {
+        if (minute >= 00 && minute <= 50) {
+            time = 1;
+        } else {
+            time = 2;
+        }
+    } else if (hour == 10) {
+        if (minute >= 00 && minute <= 50) {
+            time = 2;
+        } else {
+            time = 3;
+        }
+    } else if (hour == 11) {
+        if (minute >= 00 && minute <= 50) {
+            time = 3;
+        } else {
+            time = 4;
+        }
+    } else if (hour == 12) {
+        if (minute >= 00 && minute <= 50) {
+            time = 4;
+        } else {
+            time = 5;
+        }
+    } else if (hour == 13) {
+        if (minute >= 00 && minute <= 50) {
+            time = 5;
+        } else {
+            time = 6;
+        }
+    } else if (hour == 14) {
+        if (minute >= 00 && minute <= 40) {
+            time = 6;
+        } else {
+            time = 7;
+        }
+    } else if (hour == 15) {
+        if (minute >= 00 && minute <= 40) {
+            time = 7;
+        } else {
+            time = 8;
+        }
+    } else if (hour == 16) {
+        if (minute >= 00 && minute <= 40) {
+            time = 8;
+        } else {
+            time = 9;
+        }
+    } else if (hour > 16 || hour < 8) {
+        time = 10;
+    }
+
+    document.getElementById('time').innerText = period[time];
+
+    //다음교시까지 남은 시간 체크
+    if (time <= 6 && time > 0) {
+        leftTime = 50 - minute;
+    } else if (time > 6 && time < 10) {
+        leftTime = 40 - minute;
+    }
+    //쉬는시간이면 남은시간 출력
+    if (leftTime >= 0 && leftTime <= 9) {
+        document.getElementById('leftTime').innerText = '다음 수업까지 ' + leftTime + '분 남았어요!';
+    } else { document.getElementById('leftTime').innerText = '  '; }
+
+    getZoom(time);
+}
+//subject[요일][교시]
+
+
+//교시를 주면 링크를 가져옴
+function getZoom(time) {
+    let today = new Date();
+    let day = today.getDay();
+    console.log(day);
+    timetableNow = timetable[day][time];
+    console.log(timetableNow);
+    document.getElementById('subject').innerText = subject[timetableNow];
+
+    if (timetableNow == 4) {
+        document.getElementById('teacher').innerText = '함지* T / 원미* T';
+        document.getElementById('enterZoomBtn').innerHTML = '<div id="enterZoom"><a id="zoomLink" href="https://zoom.us/j/5977904321" target="_blank">응용프로그래밍 개발</a></div><div id="enterZoom" style="margin-top:1rem;"><a id="zoomLink" href="https://zoom.us/j/6863031340" target="_blank">정보보호 관리</a></div>'
+    } else if (timetableNow == 14) {
+        document.getElementById('teacher').innerText = teacher[timetableNow];
+    }
+    else if (timetableNow < 18) {
+        document.getElementById('teacher').innerText = teacher[timetableNow] + '* T';
+        document.getElementById('zoomLink').href = zoom + zoomId[timetableNow];
+    } else {
+        document.getElementById('teacher').innerText = teacher[timetableNow];
+        document.getElementById('enterZoomBtn').innerHTML = '';
+    }
+}
+
+function init() {
+    getTimeInfo();
+    setInterval(getTimeInfo, 1000);
+}
+
+init();
